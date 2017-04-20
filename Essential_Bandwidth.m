222768b188dc2805ee0f28c7d3b447a0f0e9a63a
adoneill03 <adoneill03@gmail.com>
2017-04-19 22:24:37 -0400

    Update Essential_Bandwidth.m

--- Essential_Bandwidth.m
+++ Essential_Bandwidth.m
@@ -6,9 +6,7 @@ function [ ESB ] = Essential_Bandwidth( Pf,percent)
 fun = matlabFunction(abs(Pf)^2);
 bw = percent*integral(fun,-Inf,Inf);
 n= .001; % resolution
-percent  = .99;
-fun = @(f) (abs(2*sinc(2*f))).^2 ;
-b1 = 0;
+b1 = 0.001;
 val = 0;
 bw = percent*integral(fun,-Inf,Inf);
 
@@ -16,7 +14,5 @@ while val<bw
    val = integral(fun,-b1,b1);
    b1 = b1+n;
 end
-disp(b1);
-disp('done');
-
+ESB = b1
 end
